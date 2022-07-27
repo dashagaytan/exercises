9ccbbbc9820f5efc1fa4f42d1b15f2a6b6577a96
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-07-27 13:32:32 -0500

    add event listener to clear-button

--- dom-olympics-start/index.js
+++ dom-olympics-start/index.js
@@ -2,6 +2,7 @@
 const h1 = document.createElement("h1")
 h1.textContent = "JavaScript Made This!!"
 h1.className = "header"
+
 document.getElementById("header").appendChild(h1);
 h1.style.textAlign = "center"
 
@@ -9,6 +10,27 @@ h1.style.textAlign = "center"
 const subTitle = document.createElement("p")
 subTitle.innerHTML += "<span style = 'color: brown' class = 'yourName'> Dasha </span> made this JavaScript"
 subTitle.className = "p"
+
 document.getElementById("header").appendChild(subTitle);
 subTitle.style.textAlign = "center"
 
+// change the context of messages 
+const newMessages = document.getElementsByClassName("message")
+newMessages[0].textContent = "Hi, lets hang out today! 🥰"
+newMessages[1].textContent = "Want to grab lunch? 🥪"
+newMessages[2].textContent = "🤩 Panera at 12? "
+newMessages[3].textContent = "Can't wait! 🤤"
+
+// loop through messages, add event listener to clear-button
+for (i = 0; i < newMessages.length; i++){
+    console.log(newMessages[i]);
+}
+
+const clearButton = document.querySelector("#clear-button")
+clearButton.addEventListener("click", function(event){
+    for (i = 0; i < newMessages.length; i++){
+        newMessages[i].remove();
+    }
+})
+
+// 
