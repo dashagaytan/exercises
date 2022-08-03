04b3cba49ae5c75666bb04a26588a2e26193e510
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-08-02 20:54:46 -0500

    loop through an array to find the ammount of comps

---
+++ loops-and-arrays/main.js
@@ -0,0 +1,9 @@
+// loop through an array and count how many 'computers' there are
+var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer",  "computer"]
+
+for(var i = 0; i < officeItems.length; i++){
+    if(officeItems[i]==="computer"){
+        console.log(officeItems[i])
+    }
+}
+
