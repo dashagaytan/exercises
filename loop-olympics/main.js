bc91d49cfad952e05373ba796e41a0cbff0cf1ce
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-08-02 22:33:28 -0500

    loop that decrements numbers

--- loop-olympics/main.js
+++ loop-olympics/main.js
@@ -1,6 +1,16 @@
 //for loop that prints out numbers 1-9
 const numbers = [1,2,3,4,5,6,7,8,9];
 
-for(const i = 0; i < numbers.length; i++){
+for(let i = 0; i < numbers.length; i++){
     console.log(numbers[i]);
+}
+
+// for loop that decrements 9-0
+
+
+// for loop that prints an array of fruits
+const fruit = ["banana", "orange", "apple", "kiwi"]
+
+for (let f = 0; f < fruit.length; f++){
+    console.log(fruit[f]);
 }
