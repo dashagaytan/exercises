438b55f81520f62a5902dcb4d560c9acfa61965b
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-08-03 15:48:30 -0500

    added html file

--- loop-olympics/main.js
+++ loop-olympics/main.js
@@ -1,16 +1,18 @@
+//Preliminaries
 //for loop that prints out numbers 1-9
 for(let i = 0; i < 10; i++){
     console.log(i);
 }
 
 // for loop that decrements 9-0
-for(let i = 10; i >= 0; i--){
+for(let i = 9; i >= 0; i--){
     console.log(i);
 }
 
 // for loop that prints an array of fruits
 const fruit = ["banana", "orange", "apple", "kiwi"]
-
-for (let f = 0; f < fruit.length; f++){
-    console.log(fruit[f]);
+for (let i = 0; i < fruit.length; i++){
+    console.log(fruit[i]);
 }
+
+//Bronze
