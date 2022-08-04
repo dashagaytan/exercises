1485ad572c0943953a98b656af1970973ca01594
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-08-03 22:47:59 -0500

    add and remove array items

---
+++ array-methods/main.js
@@ -0,0 +1,18 @@
+var fruit = ["banana", "apple", "orange", "watermelon"];
+var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
+
+// 1. Remove the last item from the vegetable array.
+vegetables.pop()
+console.log(vegetables)
+// 2. Remove the first item from the fruit array.
+fruit.shift()
+console.log(fruit)
+// 3. Find the index of "orange."
+const orangeIndex
+// 4. Add that number to the end of the fruit array.
+// 5. Use the length property to find the length of the vegetable array.
+// 6. Add that number to the end of the vegetable array.
+// 7. Put the two arrays together into one array. Fruit first. Call the new Array "food".
+// 8. Remove 2 elements from your new array starting at index 4 with one method.
+// 9. Reverse your array.
+// 10. Turn the array into a string and return it.
