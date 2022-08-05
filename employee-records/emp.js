60b613bb16bba4dc26a37d048b218cc3989bcdc0
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-08-05 17:55:24 -0500

    create 3 employee objects

--- employee-records/emp.js
+++ employee-records/emp.js
@@ -5,7 +5,15 @@ function Employee (name, jobTitle, salary, status) {
         this.name = name;
         this.jobTitle = jobTitle;
         this.salary = salary;
-        this.status = "Full Time";
+        this.status = status;
+        this.newStatus = function(){
+            console.log(this.status)
+        }
     }
 
 
+const employee1 = new Employee ('Dasha', 'VSchool', '5000/hr',)
+console.log(employee1)
+
+const employee2 = new Employee('Bob', 'Builder', '40/house', "part time")
+console.log(employee2)
