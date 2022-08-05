55324f5a8199980c419325452c67d41099236ab9
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-08-05 17:44:43 -0500

    create Employee object

---
+++ employee-records/main.js
@@ -0,0 +1,11 @@
+// collection of employee's information
+const employees = [];
+
+function Employee (name, jobTitle, salary, status) {
+        this.name = name;
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+        this.status = "Full Time";
+    }
+
+
