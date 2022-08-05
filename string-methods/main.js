073aa99183f91a7eb222665f14fe20c9124e9e07
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-08-05 11:14:06 -0500

    slice method

--- string-methods/main.js
+++ string-methods/main.js
@@ -3,3 +3,19 @@ const capAndLowerCase = ("HelLo")
 const allUpperCase = capAndLowerCase.toUpperCase()
 const allLowerCase = allUpperCase.toLowerCase()
 console.log(allUpperCase + allLowerCase)
+
+// return a number that is half he string's length, rounded down
+const hello = ("Hello")
+const indexOfhello = Math.floor(hello.indexOf("l"))
+console.log(indexOfhello)
+
+const helloWorld = ("Hello World")
+const indexOfHelloWorld = Math.floor(helloWorld.indexOf(" "))
+console.log(indexOfHelloWorld)
+
+// slice function 
+const firstLice = hello.slice(0,2)
+console.log(firstLice)
+
+const secondSlice = helloWorld.slice(0,5)
+console.log(secondSlice)
