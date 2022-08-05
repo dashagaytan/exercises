17fd74dd9987085d1577fa2d885ab56a2f117217
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-08-05 11:06:51 -0500

    string methods

---
+++ string-methods/main.js
@@ -0,0 +1,5 @@
+// return a string in all capital letters followed by all lowercase letters
+const capAndLowerCase = ("HelLo")
+const allUpperCase = capAndLowerCase.toUpperCase()
+const allLowerCase = allUpperCase.toLowerCase()
+console.log(allUpperCase + allLowerCase)
