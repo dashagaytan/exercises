3049ef70e1bc01e77cb61498cbcd3c090e29ba78
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-08-15 12:56:07 -0500

    fizzbuzz coding chanllenge

--- bugs-on-a-plane/app.js
+++ bugs-on-a-plane/app.js
@@ -1,28 +1,35 @@
 const form = document["my-form"];
+const submit = document.getElementById("submit");
 
-form.addEventListener("submit", (event) => {
-    var firstName = form.elements.value;
-    var lastName = form.elements["lastName"].value;
-    var age = form.elements.value;
-    var gender = form.elements.value;
-    var location = form.elements.value;
-    var diet = [];
+function formAlert(){
+    var firstName = form.firstName.value;
+    var lastName = form.lastName.value;
+    var age = form.age.value;
+    var gender = form.gender.value;
+    var location = form.travelLocation.value;
+    const diet = [];
 
-    if (form.vegan.checked) {
-        diet.push(document.getElementById("vegan").value);
+    // for loop 
+    for(let i = 0; i < form.check.lengh; i ++){
+        if (form.check[i].checked === true){
+            diet.push(form.check[i].value);
+        }
     }
+    // }
+    // if (form.vegan.checked) {
+    //     diet.push(document.getElementById("vegan").value);
+    // }
 
-    if (form.gluten.checked) {
-        diet.push(document.getElementById('gluten').value);
-    }
+    // if (form.gluten.checked) {
+    //     diet.push(document.getElementById("gluten").value);
+    // }
 
-    if (form.paleo.checked) {
-        diet.push(document.getElementById('paleo').value);
-    }
-
+    // if (form.paleo.checked) {
+    //     diet.push(document.getElementById("paleo").value);
+    // }
 
     alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + 
     "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
+}
 
-
-});
+submit.addEventListener("click", formAlert);
