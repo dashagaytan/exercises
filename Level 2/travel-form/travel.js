30cac6c26d60a6413b4a854c1076bc157896d2af
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-09-10 15:15:08 -0500

    styling

--- Level 2/travel-form/travel.js
+++ Level 2/travel-form/travel.js
@@ -3,5 +3,10 @@ const form = document['travelForm'];
 form.addEventListener("submit", (event)=>{
     event.preventDefault()
 
+    const checkedDiet = []
+    for( const i=0; i< checkedDiet.length; i++){
+        checkedDiet.push(checkedDiet[i].value);
+    }
+
     
 })
