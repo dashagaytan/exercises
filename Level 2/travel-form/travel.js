1fe95aeca142892a72d8d71b663ab0be665fcd32
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-09-17 15:34:42 -0500

    user clicks counter

--- Level 2/travel-form/travel.js
+++ Level 2/travel-form/travel.js
@@ -1,12 +1,25 @@
-const form = document['travelForm'];
+const form = document['travelForm']
 
-form.addEventListener("submit", (event)=>{
-    event.preventDefault()
+form.addEventListener('submit', (event)=>{
+    event.preventDefault();
 
-    const checkedDiet = []
-    for( const i=0; i< checkedDiet.length; i++){
-        checkedDiet.push(checkedDiet[i].value);
-    }
+    const fName = form.firstName.value;
+    const lName = form.lastName.value;
+    const age = form.age.value;
+    const gender = form.gender.value;
+    const city = form.city.value;
+    const diet = [];
 
-    
+   if(form.vegetarian.checked){
+        diet.push(document.getElementById('vegetarian').value);
+   } if(form.kosher.checked){
+        diet.push(document.getElementById('kosher').value);
+   } if(form.lactoseFree.checked){
+        diet.push(document.getElementById('lactoseFree').value);
+   } if(form.noRestrictions.checked){
+        diet.push(document.getElementById('noRestrictions').value)
+   }
+
+    alert ("First Name: " + fName + "\n Last Name: " + lName + "\n Age: "
+    + age + "\n Gender: " + gender + "\n City: " + city + "\n Meal: " + diet);
 })
