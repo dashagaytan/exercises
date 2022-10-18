c64fb1adbaf7a1dab9cda0a249cb731a14cbdc9f
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-10-18 13:07:41 -0500

    JSX practice with React

--- Level 3/first-React/index.js
+++ Level 3/first-React/index.js
@@ -1,1 +1,14 @@
-ReactDOM.render(<h1>Hello everyone</h1>, document.getElementById("root"))
+// ReactDOM.render(<h1>Hello everyone</h1>, document.getElementById("root"))
+
+const nav = (
+    <nav>
+        <h1>Dasha's Caffee</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(nav, document.getElementById("root"))
