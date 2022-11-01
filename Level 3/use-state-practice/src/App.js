3d59dfb14a64af640a0744f8eb1313a06f7e3f97
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-10-31 20:57:16 -0500

    useState practice init commit

---
+++ Level 3/use-state-practice/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import FaveColor from "./faveColor"
+
+export default function App() {
+     return(
+      <div>
+        <FaveColor />
+      </div>
+     )
+      
+}
+
---
+++ Level 3/use-state-practice/src/faveColor.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function FaveColor(){
+    const [color, setColor] = React.useState("pink");
+    return(
+        <div>
+            <h1>Favorite Color {color}</h1>
+            <button type="button" onClick={()=> setColor("blue")}>Click</button>
+        </div>
+    )
+}
+
