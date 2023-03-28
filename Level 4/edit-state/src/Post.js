d0e2512feb3b150780e5310e3eaf590353c0800c
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-03-27 23:28:49 -0500

    set up of handleChange and handleSubmit functions

--- Level 4/edit-state/src/Post.js
+++ Level 4/edit-state/src/Post.js
@@ -3,23 +3,46 @@ import React, {useState} from "react";
 function Post(props){
     const [showForm, setShowForm] = React.useState(false)
 
+    //state that handles edit's form
+    const [postInput, setPostInput] = useState({
+        title: "",
+        description: ""
+    })
+
     function editToggle(){
         setShowForm(prevState => !prevState)
+    }
+
+    //onChange handler
+    const handleChange =(e)=>{
+        const {name, value} = e.target
+        setPostInput(prevState =>{
+            return {
+                ...prevState,
+                [name]: value      
+            }
+    })}
+
+    const handleSubmit =(e)=>{
+        e.preventDefault()
+      
     }
 
 return(
     <div className="blog-post">
         {showForm ? 
-        <form>
+        <form onSubmit={handleSubmit}>
             <input
                 name="title"
-                value={} 
-                onChange={}/>
+                value={postInput.title} 
+                onChange={handleChange}
+                />
 
             <input
                 name="description"
-                value={} 
-                onChange={}/>    
+                value={postInput.description} 
+                onChange={handleChange}
+                />    
         </form>
         // <p>show form here</p> 
         :
@@ -28,9 +51,8 @@ return(
         <p>{props.description}</p>
      </>
     }
-        <button onClick={editToggle}>{showForm ? "Close" : "Edit"}</button>
+        <button onClick={editToggle}>{showForm ? "Save" : "Edit"}</button>
     </div>
 )
 }
-
 export default Post;
