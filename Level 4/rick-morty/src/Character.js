da9ef80ae17e75324b4b2744e8999726ad827ae1
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-03-29 00:07:08 -0500

    edit state created and edit input field

--- Level 4/rick-morty/src/Character.js
+++ Level 4/rick-morty/src/Character.js
@@ -1,13 +1,47 @@
 // Character components holds each individual character from the API
-import React,{useState, useEffect} from "react";
+import React,{useState} from "react";
 
 function Character(props){
+
+    const [isEditOn, setIsEditOn] = useState(false)
+    const [formInputs, setFormInputs] = useState({
+        firstName: ""
+    })
+
+    function toggleEdit(){
+        setIsEditOn(prevState=>!prevState)
+    }
+    
+    function handleChange(e){
+        const {name, value} = e.target
+        setFormInputs(prevState=>{
+            return{
+                ...prevState,
+                [name]:value
+            }
+        })
+    }
+
     return(
-        <>
-            <h2>{props.name}</h2>
-            <p>{props.id}</p>
+        <div>
+            {isEditOn ? 
+                <form>
+                    <input 
+                    name ="firstName"
+                    value ={formInputs.firstName}
+                    onChange={handleChange}
+                    />
+                </form>
+                :
+                <>
+                    <h2>{props.name}</h2>
+                </>
+            }
+            {/* <p>{props.id}</p> */}
             <img  src={props.image} alt=""/>
-        </>
+
+            <button onClick={toggleEdit}>{isEditOn ? "Close" : "Edit"}</button>
+        </div>
     )
 }
 
