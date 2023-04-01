35139a0ff2167a9e5dc7bba41fe374f26b533f92
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-03-31 21:31:46 -0500

    adding new value to complex state object.

--- Level 4/complex-state/src/App.js
+++ Level 4/complex-state/src/App.js
@@ -4,11 +4,22 @@ function App() {
 
   //create state to store usernames
   const [username, setUsername] = useState("bob-bilby")
-  const [names, setNames] = useState(["Lord", "Far", "Quad"])
+  const [names, setNames] = useState(["Lord", "Far", "Quaad"])
   const [style, setStyle] = useState({
     backgroundColor: "pink",
     width: "300px",
     fontSize: "20px"
+  })
+
+  const [person, setPerson] = useState({
+    name: "John",
+    age: "60",
+    address: {
+      street: "whipple",
+      city: "Tree",
+      state: "CA"
+    },
+    favoriteFood: ["pizza", "tacos"]
   })
 
   console.log("App component rendered")
@@ -16,7 +27,7 @@ function App() {
   function changeUsername(){
     console.log("changeName function ran")
     //1. changing state by passing in new state value
-    setUsername("chily-healer")
+    setUsername("Lord Farquuad-healer")
 
   }
 
@@ -24,7 +35,7 @@ function App() {
     //2. passing in a callback function that will
     // give access to previous state
     // overwrite, and add the new state
-    setNames((prevState)=> {
+    setNames((prevState)=> {  //explisit return 
       return [...prevState, username]
 
     })
@@ -50,6 +61,27 @@ function App() {
     })
   }
 
+  function changeStreet(){
+    setPerson((prevState)=> {
+      return{
+        ...prevState,
+        address: {
+          ...prevState.address,
+          street: "Cat ln"
+        }
+      }
+    })
+  }
+
+  function addFood(){
+    setPerson((prevState)=>{
+      return{
+        ...prevState,
+        favoriteFood: [...prevState.favoriteFood, "ice-cream"]  //we spread prev items in the array and hardcode new fave food to the array.
+      }
+    })
+  }
+
   //mapping through the names state and render each name in a paragraph tag 
   const listNames = names.map((name, index)=> {
     return <p>{name}</p>
@@ -57,13 +89,19 @@ function App() {
 
   return (
     <div className="App" style ={style}>
-      <h2>My name is: {username}</h2>
+      <h2 style={{color: "green"}}>My name is: {username}</h2>
       <h2>List of names: </h2>
       {listNames}
       <button onClick={changeUsername}>Change username</button>
-      <button onClick={addName}>Add Username</button>
+      <button  onClick={addName}>Add Username</button>
       <button onClick={changeOrange}>Change Background Color to orange</button>
+      <br>
+      </br>
       <button onClick={toggleBackground}>Toggle Background color</button>
+      <button onClick={changeStreet}>Change Street Name</button>
+      <br>
+      </br>
+      <button onClick={addFood}>Add Food</button>
     </div>
   );
 }
