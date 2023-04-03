dd4c385987eeee1827a2218de4d666e2a074b407
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-04-03 16:21:51 -0500

    change state onClick

--- Level 4/state-intro/src/App.js
+++ Level 4/state-intro/src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 function App() {
+
+  
+  //with useState you pass in an initial value. data type: (string, object, array...)
+  const [name, setName] = useState("John Doe")
+  console.log(`State: ${name}`)
+
+  const changeName = () =>{
+    setName(prevState => prevState === "John Doe" ? " Bob Smith" : "John Doe")
+    console.log(changeName)
+  }
+
   return (
-    <div className="App">
-     
+    <div className="app" style={{border: "1px solid purple", background: "pink", width: "300px", padding: "20px"}}> 
+    <h1>{name}</h1>
+    <button onClick={changeName}>Change Name</button>
+
     </div>
   );
 }
