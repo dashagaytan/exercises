cbf154c554984d9b32ab71fb993c60d643fc84af
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-04-04 11:56:20 -0500

    adding submit form and managing hardcoded data

--- Level 4/components-state-edit/src/App.js
+++ Level 4/components-state-edit/src/App.js
@@ -1,12 +1,102 @@
-import React from "react";
-import ContactList from "./ContactList"
+import React, {useState} from "react";
+import Contact from "./Contact"
 
 function App() {
 
+  const [name, setName] = useState({
+    name: "",
+    number: ""
+  })
+
+  const data = [
+    {
+      id: 0,
+      name: "John Doe",
+      number: "888-333-2211"
+    },
+    {
+      id: 1,  
+      name: "Jane Doe",
+      number: "777-333-2211"
+    },
+    {
+      id: 3,
+      name: "Jo Doe",
+      number: "999-333-2211"
+    }
+  ]
+
+  function addName(){
+    let newPerson = {
+      name: "",
+      number: ""
+    }
+    setName(prevName => {
+      return {
+        data:
+        [...prevName, newPerson]
+      }
+    })
+  }
+
+//add a new name and number to the list using a Form
+  function handleChange (e){
+    const {value, name} = e.target
+    setName(prevState => {
+      return{
+        ...prevState,
+        [name]: value
+      }
+    })
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    setName(prevState=> ({
+      data: [
+        ...prevState.data, 
+        {name: prevState.name, number: prevState.number}],
+      name: "",
+      number: ""
+    }))
+  }
+
+  const listContacts = data.map((item, id)=>{
+    return (
+      <Contact
+        name = {item.name}
+        number = {item.number}
+        key={item.id}
+      />
+
+    )
+  })
+
   return (
-    <div className="App">
-      <ContactList/>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input 
+        name ="name"
+        value = {name.name}
+        onChange={handleChange}
+        placeholder="Name"/>
+        
+        <input 
+        name ="number"
+        value = {name.number}
+        onChange={handleChange}
+        placeholder="Number"/>
+
+        <button>Submit</button>
+      </form>
+  
+      <div className="App">
+        {listContacts}
+        <Contact />
+        <button onClick={addName}>Add Name</button>
+      </div>
+    </>
+  
   );
 }
 
