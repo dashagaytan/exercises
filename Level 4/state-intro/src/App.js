792364b99c01f317ce1d638accc510b4ada9ac64
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-04-05 18:54:55 -0500

    initial setup

--- Level 4/state-intro/src/App.js
+++ Level 4/state-intro/src/App.js
@@ -6,6 +6,22 @@ function App() {
   const [name, setName] = useState(["Bob", "Joe", "Jane"])
   console.log(`State: ${name}`)
 
+  const [person, setPerson] = useState({
+    firstName: "Dasha",
+    lastName: "Gaytan",
+    faveColor: "pink"
+  })
+
+  const [people, setPeople] = useState([{
+    firstName: "David",
+    lastName: "Gaytan",
+    faveColor: "blue"
+  }])
+
+  function addPerson(){
+    setPeople(prevState => {return [...prevState, ...person]})
+  }
+
   const changeName = () =>{
     setName(prevState => prevState === "John Doe" ? " Bob Smith" : "John Doe")
     console.log(changeName)
@@ -62,30 +78,40 @@ function App() {
     <img src={item.imgUrl} alt="" style={{height: "200px"}}/>
   </div>)
 
+  const personList = person.map((human, index)=>
+  <div key={index+human.index}>
+    <h3 style={{color: person.faveColor}}>First Name: {human.firstName}</h3>
+    <h3>Last Name: {human.lastName}</h3>
+  </div>)
+
   return (
     <div className="app" style={{background: "pink", width: "400px", padding: "20px"}}> 
-    <h1 style={{color: "rgb(230, 120, 196)"}}>{name}</h1>
-    <button onClick={changeName}>Change Name</button>
-    <button onClick={addName}>Add Name</button>
-<hr>
-</hr>
-    <form onSubmit={handleSubmit}>
-      <input 
-      name = "firstName"
-      value = {formInfo.firstName}
-      placeholder="Name"
-      onChange={handleChange}
-      />
-      <input
-      name = "imgUrl"
-      value = {formInfo.imgUrl}
-      placeholder="Image Url "
-      onChange={handleChange}
-      />
-      <button>Submit</button>
+      <h1 style={{color: "rgb(230, 120, 196)"}}>{name}</h1>
+      <button onClick={changeName}>Change Name</button>
+      <button onClick={addName}>Add Name</button>
 
-    </form>
-    {listOfItems}
+  <hr>
+  </hr>
+      <form onSubmit={handleSubmit}>
+        <input 
+        name = "firstName"
+        value = {formInfo.firstName}
+        placeholder="Name"
+        onChange={handleChange}
+        />
+        <input
+        name = "imgUrl"
+        value = {formInfo.imgUrl}
+        placeholder="Image Url "
+        onChange={handleChange}
+        />
+        <button>Submit</button>
+
+      </form>
+
+      {listOfItems}
+
+      {personList}
     </div>
   );
 }
