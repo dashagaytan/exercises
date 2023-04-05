66ac93206dae3a3f785c7288abbdefb11708a000
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-04-04 20:27:31 -0500

    submiting and clearing the input form

--- Level 4/state-intro/src/App.js
+++ Level 4/state-intro/src/App.js
@@ -16,10 +16,25 @@ function App() {
     imgUrl: ""
   })
 
+  const [list, setList] = useState([])
+
+  //function that submits user inputs to a list
   const handleSubmit = (e) => {
     e.preventDefault();
-
-
+    setList(prevState => {
+      return [
+        ...prevState,
+        {
+          firstName: formInfo.firstName,
+          imgUrl: formInfo.imgUrl
+        }
+      ]
+    })
+    //to clear the inputs after form was submitted 
+    setFormInfo({
+      firstName: "", 
+      imgUrl: ""
+    })
   }
 
   const handleChange = (e) => {
@@ -31,6 +46,14 @@ function App() {
       }
     })
   }
+
+  //mapping through a list of inputs and displaying them in DOM. 
+  //each child component rendered must have a key identigier (index + item.firstName). if call from API we use id as key
+  const listOfItems = list.map((item, index) => 
+  <div key={index+item.firstName}>
+    <p>First Name: {item.firstName}</p>
+    <img src={item.imgUrl} alt="" style={{height: "200px"}}/>
+  </div>)
 
   return (
     <div className="app" style={{background: "pink", width: "400px", padding: "20px"}}> 
@@ -54,6 +77,7 @@ function App() {
       <button>Submit</button>
 
     </form>
+    {listOfItems}
     </div>
   );
 }
