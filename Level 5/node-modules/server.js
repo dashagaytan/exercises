513947fea3ef18f1b7204f5dda5fec3274674a15
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-05-19 12:05:20 -0500

    commit

--- Level 5/node-modules/server.js
+++ Level 5/node-modules/server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const {v4: uuidv4} = require('uuid')
 
 const app = express()
 
@@ -6,11 +7,11 @@ app.use(express.json())
 
 //Fake data: 
 const movies = [
-    { title: "Titanic", genre: "drama" },
-    { title: "Spider Man", genre: "action" },
-    { title: "Lion King", genre: "fantasy" },
-    { title: "Star Wars", genre: "fantasy" },
-    { title: "Orphin", genre: "horror" }
+    { title: "Titanic", genre: "drama", _id: uuidv4()  },
+    { title: "Spider Man", genre: "action", _id: uuidv4() },
+    { title: "Lion King", genre: "fantasy", _id: uuidv4() },
+    { title: "Star Wars", genre: "fantasy", _id: uuidv4() },
+    { title: "Orphin", genre: "horror", _id: uuidv4() }
 ]
 
 // get request 
@@ -18,8 +19,10 @@ app.get("/movies", (req, res)=> {
     res.send(movies)
 })
 
+//when adding a new movie we need to assign it an _id
 app.post("/movies", (req, res)=>{
     const newMovie = req.body
+    newMovie._id = uuidv4()
     movies.push(newMovie)
     res.send(`Added ${newMovie.title} to our Database`)
 })
@@ -28,3 +31,5 @@ app.post("/movies", (req, res)=>{
 app.listen(9000, () => {
     console.log("The server is running on port 9000")
 })
+
+
