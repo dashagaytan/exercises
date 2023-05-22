339f390fa34e61d81908c9a090cee8a7b4008652
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-05-22 13:18:40 -0500

    middleware  and next

--- Level 5/node-modules/server.js
+++ Level 5/node-modules/server.js
@@ -5,6 +5,18 @@ const app = express()
 //Middleware (for every request)
 app.use(express.json())  //Looks for a request body, and turns it inot 'req.body'
 
+app.use("/items", (req, res, next)=>{
+    console.log("The items middleware was executed")
+    next()
+})
+app.get("/items", (req, res, next)=>{
+    req.body = {name: "Rick"}
+    next()
+})
+app.get("/items", (req, res, next)=> {
+    res.send(req.body)
+})
+
 // Routes we tell the app to use our routes that we have created in order for the server to see it.
 app.use("/movies", require("./routes/movieRoutes.js"))
  
