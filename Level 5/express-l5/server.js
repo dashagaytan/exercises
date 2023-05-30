6e4b8dbeabe867f4c4ae60c6a0aaa4b8d692bcb9
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-05-30 15:05:38 -0500

    installed and connected mongodb

--- Level 5/express-l5/server.js
+++ Level 5/express-l5/server.js
@@ -1,10 +1,18 @@
 const express = require("express")
 const morgan = require('morgan')
 const app = express()
+const mongoose = reqire("mongoose")
 
 //Middleware (for every request)
 app.use(express.json())  // Looks for a request body, and turns it inot 'req.body'
 app.use(morgan('dev')) // Logs requests to the console
+
+// Connect to DB, db default port is 27017
+mongoose.connect('mongodb://localhost:27017/moviesdb', 
+{
+
+},
+)
 
 // Routes we tell the app to use our routes that we have created in order for the server to see it.
 app.use("/movies", require("./routes/movieRoutes.js"))
